81d813694cc71063b570aafacb8f2075b4fe3f43
jheni.fermino <163904723+rainefermino@users.noreply.github.com>
2025-03-11 16:03:05 -0300

    11-03

--- scrpt.js
+++ scrpt.js
@@ -2,8 +2,9 @@ document.addEventListener('DOMContentLoaded',function(){
 const aumentaFonteBotao=document.getElementById('aumentar-fonte');
 
 let tamanhoAtualfonte=1;
-
-
-
+aumentaFonteBotao.addEventListener('click',function(){
+    tamanhoAtual +=0.1;
+    document.body.style.fontSize=`${tamanhoAtualfonte}rem`
+})
 
 })
